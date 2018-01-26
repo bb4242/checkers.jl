ca2ab4763b336df85e4ddb0a45955c87a635496b
Brett Bethke <bbethke@airware.com>
2018-01-25 17:01:45 -0800

    Basic tic tac toe functions implemented

---
+++ ttt.jl
@@ -0,0 +1,61 @@
+module TTT
+
+@enum TURN p1turn=1 p2turn=2
+@enum BOARD empty=0 player1=1 player2=2
+@enum TERMINAL nonterminal=0 p1wins=1 p2wins=2
+
+struct State
+    turn::TURN
+    board::Array{BOARD, 2}
+end
+
+State() = State(p1turn, fill(empty, 3, 3))
+
+struct Move
+    x::Int8
+    y::Int8
+end
+
+function move(s::State, m::Move)
+    @assert s.board[m.x, m.y] == empty
+    new_board = deepcopy(s.board)
+    new_board[m.x, m.y] = s.turn == p1turn ? player1 : player2
+    new_turn = s.turn == p1turn ? p2turn : p1turn
+    State(new_turn, new_board)
+end
+
+function valid_moves(s::State)
+    nx, ny = size(s.board)
+    moves::Array{Move, 1} = []
+    for x=1:nx, y=1:ny
+        if s.board[x, y] == empty
+            push!(moves, Move(x, y))
+        end
+    end
+    moves
+end
+
+function is_terminal(s::State)
+    if _check_array(s.board .== player1) return p1wins end
+    if _check_array(s.board .== player2) return p2wins end
+    return nonterminal
+end
+
+function _check_array(a::BitArray{2})
+    nx, ny = size(a)
+    for x=1:nx
+        if all(a[x, :]) return true end
+    end
+
+    for y=1:ny
+        if all(a[:, y]) return true end
+    end
+
+    if all(a[[CartesianIndex(i, i) for i=1:nx]]) return true end
+    if all(a[[CartesianIndex(i, nx+1-i) for i=1:nx]]) return true end
+
+    return false
+end
+
+
+end
