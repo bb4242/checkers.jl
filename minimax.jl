934c16971b3baa25b99a9a3b7a1abb6cd90249f7
Brett Bethke <bbethke@airware.com>
2018-02-01 16:15:01 -0800

    Add game playing interface

--- minimax.jl
+++ minimax.jl
@@ -162,7 +162,8 @@ function test()
     b = [O q O;
          q X q;
          q q X]
-    s = State(p1turn, b)
+    s = State(p2turn, b)
+#    s = State()
     r, n = mcts(s, 10000)
 
     display(b)
@@ -177,7 +178,42 @@ end
 end
 
 
-r, n = MCTS.test();
+module Game
+
+using TTT
+using MCTS
+
+function play_game()
+    state = State()
+    computer_player = rand([p1turn, p2turn])
+    println("Computer is ", computer_player)
+
+    while !is_terminal(state)[1]
+        println("It's the turn of ", state.turn)
+        display(state.board)
+        println()
+
+        if state.turn == computer_player
+            r, n = MCTS.mcts(state, 100000)
+            state = MCTS.best_child(n, 0.0).board_state
+        else
+            println("\nEnter your move like (1, 3): ")
+            input = map(x->parse(Int, x), split(readline(), ","))
+            move = Move(input[1], input[2])
+            state = apply_move(state, move)
+        end
+    end
+
+    println("Game over, winner: ", is_terminal(state))
+    display(state.board)
+end
+
+
+end
+
+Game.play_game()
+
+#r, n = MCTS.test();
 
 #r = MCTS.mcts(MCTS.s, 1000)
 
