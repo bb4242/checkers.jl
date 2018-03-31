715c6b385b50960fda73bc4804b6a0e902cd312f
Brett Bethke <bbethke@airware.com>
2018-03-31 15:08:44 -0700

    Game server mostly working now

--- gameserver.jl
+++ gameserver.jl
@@ -15,41 +15,7 @@ end
 # Map from game_id to state data about that game
 games = Dict{Any, ServerGame}()
 
-# http = HttpHandler() do req::Request, res::Response
-#     global next_id
-#     global game_states
-#     println(req.resource)
-#     m = match(r"^/games/new/?$", req.resource)
-#     if m != nothing
-#         game_states[next_id] = State()
-#         next_id += 1
-#         return Response(string(next_id-1))
-#     end
-
-#     m = match(r"^/games/(\d+)/?$", req.resource)
-#     if m != nothing
-#         game_id = parse(Int, m.captures[1])
-#         return Response(200, Dict{AbstractString,AbstractString}([("Content-Type","application/json")]), JSON.json(game_states[game_id], 2))
-#     end
-
-
-
-#     #m = match(r"^/games/(\d+)/?$", req.resource)
-#     #if m == nothing return Response(404) end
-#     #println(m.captures)
-#     #number = parse(BigInt, m.captures[1])
-#     #if number < 1 || number > 100_000 return Response(500) end
-#     #return Response(string(fibonacci(number)))
-# end
-
-# http.events["error"]  = (client, err) -> println(err)
-# http.events["listen"] = (port)        -> println("Listening on $port...")
-
-# server = Server( http )
-# run( server, 8000 )
-
 function do_server()
-    println("Hello")
     while true
         server = listen(4242)
         while true
@@ -61,7 +27,7 @@ end
 
 function handle_client(sock)
     while isopen(sock)
-        println("Waiting...")
+        println(games)
         msg = readline(sock)
         println("Received: ", msg)
         try
@@ -70,8 +36,6 @@ function handle_client(sock)
             command = res["command"]
             game_id = res["game_id"]
             player = res["player"]
-            println("Command: ", command)
-            println("Games ", games)
 
             if command == "join"
                 println("PLAYER ", player)
@@ -99,28 +63,63 @@ function handle_client(sock)
                 end
 
             elseif command == "move"
-                if haskey(games, game_id) && games[game_id].players[player] == sock
-                    move = res["move"]
-                    println("Game $game_id: player $player move $move")
+                if haskey(games, game_id)
+                    game = games[game_id]
+
+                    if game.players[player] != sock
+                        write(sock, """{"result": "error", "reason": "trying to move for another player"}\n""")
+                        continue
+                    elseif length(game.players) != 2
+                        write(sock, """{"result": "error", "reason": "other player has not joined"}\n""")
+                        continue
+                    elseif game.game_state.turn != (player == 1 ? Checkers.p1turn : Checkers.p2turn)
+                        write(sock, """{"result": "error", "reason": "not your turn"}\n""")
+                        continue
+                    end
+
+                    move_arr = res["move"]
+                    println("Game $game_id: player $player move $move_arr")
+
+                    for i in 1:(length(move_arr) - 1)
+                        cm = Checkers.Move(move_arr[i][1], move_arr[i][2], move_arr[i+1][1], move_arr[i+1][2])
+                        game.game_state = Checkers.apply_move(game.game_state, cm, Checkers.CheckersMem())
+                    end
 
                     # TODO: Make sure game has 2 players
                     # TODO: Validate it's your turn
                     # TODO: Apply move
                     # TODO: Check for game that has ended & remove it from games dict
 
-                    for pair in games[game_id].players
-                        write(pair[2], """{"result": "ok", "game_state": $(JSON.json(games[game_id].game_state))}\n""")
+                    for pair in game.players
+                        write(pair[2], """{"result": "ok", "game_state": $(JSON.json(game.game_state))}\n""")
+                        if Checkers.is_terminal(game.game_state, Checkers.CheckersMem())
+                            close(pair[2])
+                        end
                     end
-
-                    continue
                 end
             end
 
         catch exc
-            println("EXC ", exc)
+            println("Exception caught: ", exc)
+            println(catch_stacktrace())
         end
     end
 
+    println("Client $sock disconnected")
+    for game_pair in games
+        game = game_pair[2]
+        players = game.players
+        for player_pair in game.players
+            if player_pair[2] == sock
+                delete!(game.players, player_pair[1])
+                if length(game.players) == 0
+                    println("Last player left $(game_pair[1])")
+                    delete!(games, game_pair[1])
+                end
+            end
+        end
+    end
+    println(games)
 
     # TODO: Remove client from game, delete game if necessary
 end
